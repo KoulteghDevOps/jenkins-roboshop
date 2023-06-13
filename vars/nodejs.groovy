9f209e6703c5700be71a53745ee19405a8f4a734
KoulteghDevOps <koultegh@outlook.com>
2023-06-13 13:28:17 +0100

    manifest updated

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -20,7 +20,7 @@ def call() {
       stage('Code Quality') {
         steps {
           sh 'ls -ltr'
-          sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.95.180:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true -Dsonar.java.binaries=./target'
+          sh 'sonar-scanner -Dsonar.projectKey=${component} -Dsonar.host.url=http://172.31.95.180:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.qualitygate.wait=true'
         }
       }
       stage('Unit Test Cases') {
